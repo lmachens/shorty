13ebf72a4adf2d51c1f393cc82f311e83e3fcce8
lmachens <leon.machens@googlemail.com>
2020-12-17 15:01:41 +0100

    ✨ Sync failed POST requests https://developers.google.com/web/tools/workbox/modules/workbox-background-sync

--- client/src/service-worker.ts
+++ client/src/service-worker.ts
@@ -12,7 +12,8 @@ import { clientsClaim } from "workbox-core";
 import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
+import { StaleWhileRevalidate, NetworkOnly } from "workbox-strategies";
+import { BackgroundSyncPlugin } from "workbox-background-sync";
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -79,3 +80,15 @@ self.addEventListener("message", (event) => {
 });
 
 // Any other custom service worker logic can go here.
+
+const bgSyncPlugin = new BackgroundSyncPlugin("postQueue", {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  /\/api\/.*\/*/,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "POST"
+);
